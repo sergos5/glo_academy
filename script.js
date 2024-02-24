97eaeac79837dd0229ae10515f77d5e11c049673
Sergey <sergos5@yandex.ru>
2024-02-24 15:36:45 +0300

    задание второго урока

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let title;
-let screens;
-let screenPrice;
-let roolback;
-let fullPrice;
-let adaptive;
-
-alert ("Привет модальное окно");
-
-console.log("Привет консоль");
-
-
+let title = 'lesson02';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 5000;
+let roolback = 15;
+let fullPrice = 200000;
+let adaptive = true;
+// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive
+console.log('Тип данных title - ' + typeof(title));
+console.log('Тип данных fullPrice - ' + typeof(fullPrice));
+console.log('Тип данных adaptive - ' + typeof(adaptive));
+// Вывести в консоль длину строки из переменной screens 
+console.log('Длина строки screens - ' + screens.length);
+// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани” 
+console.log('Стоимость верстки экранов '+ screenPrice + ' рублей.');
+console.log('Стоимость разработки сайта '+ fullPrice + ' рублей.');
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль 
+console.log(screens.toLowerCase().split(', '));
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100)) 
+console.log('Откат посреднику ' + fullPrice * (roolback/100) + ' денег');
