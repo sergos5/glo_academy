352450476264dd747ae081e320ac363c44cb1f35
Sergey <sergos5@yandex.ru>
2024-02-26 22:24:59 +0300

    задание третьего урока

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-let title = 'lesson02';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 5000;
-let roolback = 15;
-let fullPrice = 200000;
-let adaptive = true;
-// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive
-console.log('Тип данных title - ' + typeof(title));
+let title = prompt('Как называется Ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
+let roolback = 0;
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+let fullPrice = screenPrice+servicePrice1+servicePrice2;
+let servicePercentPrice  = Math.ceil(fullPrice - fullPrice * (roolback/100));
+console.log ('Итоговая стоимость работы ' + servicePercentPrice);
+
+switch (true) {
+    case fullPrice >= 30000: 
+        console.log ('Даем скидку в 10%');
+        break;
+    case fullPrice >= 15000: 
+        console.log ('Даем скидку в 5%');
+        break;
+    case fullPrice >= 0: 
+        console.log ('Скидка не предусмотрена');
+        break;
+    case fullPrice < 0: 
+        console.log ('Что то пошло не так');
+        break;
+}
+
+
+//осталось от урока 2
+/* console.log('Тип данных title - ' + typeof(title));
 console.log('Тип данных fullPrice - ' + typeof(fullPrice));
 console.log('Тип данных adaptive - ' + typeof(adaptive));
-// Вывести в консоль длину строки из переменной screens 
 console.log('Длина строки screens - ' + screens.length);
-// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани” 
 console.log('Стоимость верстки экранов '+ screenPrice + ' рублей.');
 console.log('Стоимость разработки сайта '+ fullPrice + ' рублей.');
-// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль 
 console.log(screens.toLowerCase().split(', '));
-// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100)) 
-console.log('Откат посреднику ' + fullPrice * (roolback/100) + ' денег');
+console.log('Откат посреднику ' + fullPrice * (roolback/100) + ' денег'); */
