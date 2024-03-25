40a1f593014fa339cf3401a2c9e98537ff4cd909
Sergey <sergos5@yandex.ru>
2024-03-25 12:46:50 +0300

    25.03.24

--- js/script.js
+++ js/script.js
@@ -20,7 +20,8 @@ const totalCountRollback = document.getElementsByClassName('total-input')[4];
 let screens = document.querySelectorAll('.screen');
 
 const cmsCheckbox = document.getElementById('cms-open');
-const cmsVar = document.querySelector('.hidden-cms-variants');
+const cmsVariants = document.querySelector('.hidden-cms-variants');
+//cmsVariants.style.display = 'flex';
 
 
 const appData = {
